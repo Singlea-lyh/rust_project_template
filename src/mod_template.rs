2b6361b67e6338e5ce8189881b5ca17d2f98303d
Singlea_lyh <chinese_lyh@163.com>
2024-12-20 02:36:27 +0000

    Change The Name of Modules...

---
+++ src/mod_template.rs
@@ -0,0 +1,12 @@
+pub fn add(first: i32, second: i32) -> i32 {
+    first + second
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = super::add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
