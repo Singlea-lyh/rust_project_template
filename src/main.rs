a13a68e8803e3fbb9cee34b5f41e796e715ac204
Singlea_lyh <chinese_lyh@163.com>
2024-12-20 01:25:09 +0000

    Fiist Success Commit...

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    add(2, 2);
+    println!("Hello, rust!");
+}
+
+fn add(first: i32, second: i32) -> i32 {
+    first + second
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = super::add(2, 2);
+        assert_eq!(result, 4);
+    }
 }
