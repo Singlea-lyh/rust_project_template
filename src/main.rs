6f6c4bf929b9429b9a65109a08614d82a330f3c4
Singlea_lyh <chinese_lyh@163.com>
2024-12-20 02:28:41 +0000

    Add The modules for this  Template...

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,6 @@
+use rust_template::modules::add;
+
 fn main() {
     add(2, 2);
     println!("Hello, rust!");
-}
-
-fn add(first: i32, second: i32) -> i32 {
-    first + second
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = super::add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
